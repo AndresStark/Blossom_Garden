4137cbc92428438db8d1d4ea3a2ed9a425a0d9d8
Cloud <andres.sanchez.cloud@gmail.com>
2024-02-08 21:24:11 -0600

    Testeo de vistas exitoso. El servidor se activa bien y se muestra el template designado

--- florilegio/inventory/urls.py
+++ florilegio/inventory/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-app_name = "inventory"
+app_name = "catalogo"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+]
--- florilegio/inventory/views.py
+++ florilegio/inventory/views.py
@@ -1,3 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name: str = 'index.html'
+    context_object_name = "index"
+
+    def get_queryset(self):
+        return "Hola mundo"
