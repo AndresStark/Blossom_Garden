dc0492fb096bbd7f2377206837964a6309c0be91
KarenBurgos <00043820@uca.edu.sv>
2024-02-27 03:33:41 -0300

    Creacion de proyecto react con Bootstrap

---
+++ blossom-garden-frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './custom.scss';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button className="primary">
+        bootstrap test
+      </Button>
+    </div>
+  );
+}
+
+export default App;
